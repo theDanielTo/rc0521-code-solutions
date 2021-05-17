613992b61bd307238af9cdc08109092421424433
theDanielTo <dto1989@gmail.com>
2021-05-17 10:25:59 -0700

    Completed dom-querying

---
+++ dom-querying/main.js
@@ -0,0 +1,19 @@
+var h1Element = document.querySelector('h1');
+console.log(h1Element);
+console.dir(h1Element);
+
+var explanationElement = document.querySelector('#explanation');
+console.log(explanationElement);
+console.log('.dir(explanationElement)');
+console.dir(explanationElement);
+
+var hintElement = document.querySelector('.hint');
+console.log(hintElement);
+console.log('.dir(hintElement)');
+console.dir(hintElement);
+
+var allPElements = document.querySelectorAll('p');
+console.log(allPElements);
+
+var allExampleLinkElements = document.querySelectorAll('.example-link');
+console.log(allExampleLinkElements);
