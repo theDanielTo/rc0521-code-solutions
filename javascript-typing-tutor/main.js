95c368f8a4448b569f346596a1a5bc30ec07d436
theDanielTo <dto1989@gmail.com>
2021-05-22 16:34:44 -0700

    Completed allows user to input customized phrases into typing tutor

--- javascript-typing-tutor/main.js
+++ javascript-typing-tutor/main.js
@@ -1,62 +1,70 @@
-const quotes = [
-  "Frankly, my dear, I don't give a damn.",
-  'May the force be with you.',
-  'Stella! Hey, Stellaaaaaaaaaaaaaa',
-  "Here's Johnny!",
-  'We got this team! :D'
-];
+const $inputForm = document.querySelector('.input-form');
+const $textInput = document.querySelector('input');
+const $startButton = document.querySelector('#start-btn');
+const $tutorText = document.querySelector('.tutor-text');
 
-const tutorText = document.querySelector('.incomplete');
-const completedText = document.querySelector('.complete');
-const currentLetter = document.querySelector('.current-letter');
-const finishedCard = document.querySelector('.finished-card');
-const accuracyText = document.querySelector('#accuracy');
-const playAgainButton = document.querySelector('button');
+const $incompleteText = document.querySelector('.incomplete');
+const $completedText = document.querySelector('.complete');
+const $currentLetter = document.querySelector('.current-letter');
+
+const $finishedCard = document.querySelector('.finished-card');
+const $accuracyDiv = document.querySelector('.typing-acc');
+const $accuracyText = document.querySelector('#accuracy');
+const $playAgainButton = document.querySelector('#play-again-btn');
+
 let incorrectPresses = 0;
-let textLength = tutorText.textContent.length + 1;
+let textLength = 1;
+
+$startButton.addEventListener('click', function (event) {
+  $currentLetter.textContent = $textInput.value[0];
+  $incompleteText.textContent = $textInput.value.slice(1);
+  textLength = $incompleteText.textContent.length + 1;
+  $inputForm.className = 'input-form hidden';
+  $tutorText.className = 'tutor-text';
+});
 
 document.addEventListener('keydown', function (event) {
-  if (tutorText.textContent.length === 0) {
-    document.addEventListener('keydown', e => {
-      if (e.key === 'Enter') {
-        finishedCard.className = 'finished-card hidden';
-        resetText();
-      }
-    });
-    finishedCard.className = 'finished-card';
-    completedText.textContent += currentLetter.textContent;
-    currentLetter.textContent = '';
-    tutorText.textContent = '';
-  } else if (event.key === currentLetter.textContent) {
-    currentLetter.className = '';
-    completedText.textContent += currentLetter.textContent;
-    currentLetter.textContent = tutorText.textContent[0];
-    tutorText.textContent = tutorText.textContent.slice(1);
+  const hasntStarted = $currentLetter.textContent.length !== 0;
+  if ($incompleteText.textContent.length === 0 && hasntStarted) {
+    $finishedCard.className = 'finished-card';
+    $accuracyDiv.className = 'typing-acc';
+    $completedText.textContent += $currentLetter.textContent;
+    $currentLetter.textContent = '';
+    $incompleteText.textContent = '';
+  } else if (event.key === $currentLetter.textContent && hasntStarted) {
+    $currentLetter.className = '';
+    $completedText.textContent += $currentLetter.textContent;
+    $currentLetter.textContent = $incompleteText.textContent[0];
+    $incompleteText.textContent = $incompleteText.textContent.slice(1);
   } else {
-    if (event.key !== 'Shift') {
-      currentLetter.className = 'incorrect';
+    if (event.key !== 'Shift' && hasntStarted) {
+      $currentLetter.className = 'incorrect';
       incorrectPresses++;
-      accuracyText.textContent = nearestHundredth(((textLength - incorrectPresses) / textLength * 100)) + '%';
+      $accuracyText.textContent = calcAcc(textLength, incorrectPresses);
     }
   }
 });
 
-playAgainButton.addEventListener('click', function (event) {
-  finishedCard.className = 'finished-card hidden';
+$playAgainButton.addEventListener('click', function (event) {
+  $finishedCard.className = 'finished-card hidden';
+  $accuracyDiv.className = 'typing-acc hidden';
+  $inputForm.className = 'input-form';
+  $tutorText.className = 'tutor-text hidden';
+  $textInput.focus();
   resetText();
 });
 
-function nearestHundredth(x) {
-  if (x <= 0) return 0;
-  return Number.parseFloat(x).toFixed(2);
+function calcAcc(length, presses) {
+  if (length - presses <= 0) return 0 + '%';
+  return Number.parseFloat(((textLength - incorrectPresses) / textLength * 100).toFixed(2)) + '%';
 }
 
 function resetText() {
   incorrectPresses = 0;
-  const randomIndex = Math.floor(Math.random() * quotes.length);
-  completedText.textContent = '';
-  currentLetter.textContent = quotes[randomIndex][0];
-  tutorText.textContent = quotes[randomIndex].slice(1);
-  textLength = tutorText.textContent.length + 1;
-  accuracyText.textContent = 100 + '%';
+  $completedText.textContent = '';
+  $currentLetter.textContent = '';
+  $incompleteText.textContent = '';
+  textLength = 0;
+  $textInput.value = '';
+  $accuracyText.textContent = 100 + '%';
 }
