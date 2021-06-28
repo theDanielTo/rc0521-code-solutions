6f71e23ea6c64871d0fad5c757e8088cdb48dfcb
theDanielTo <dto1989@gmail.com>
2021-06-28 16:18:10 -0700

    Completed express-delete

---
+++ express-delete/index.js
@@ -0,0 +1,38 @@
+const express = require('express');
+const app = express();
+const grades = {
+  12: {
+    id: 12,
+    name: 'Tim Berners-Lee',
+    course: 'HTML',
+    score: 95
+  },
+  47: {
+    id: 47,
+    name: 'Brendan Eich',
+    course: 'JavaScript',
+    score: 100
+  },
+  273: {
+    id: 273,
+    name: 'Forbes Lindsay',
+    course: 'JavaScript',
+    score: 92
+  }
+};
+
+app.get('/api/grades', (req, res) => {
+  const gradesArr = [];
+  for (const key in grades) gradesArr.push(grades[key]);
+  res.json(gradesArr);
+});
+
+app.delete('/api/grades/:id', (req, res) => {
+  delete grades[req.params.id];
+  res.sendStatus(204);
+});
+
+app.listen(3000, () => {
+  // eslint-disable-next-line no-console
+  console.log('Listening in port 3000.');
+});
