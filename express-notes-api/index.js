ed3642fa42556eb1d3e5864e9f8983778b9333c5
theDanielTo <dto1989@gmail.com>
2021-06-29 15:37:41 -0700

    Client can post a new note. Client can delete a note. Need to send 500 status for unexpected errors

--- express-notes-api/index.js
+++ express-notes-api/index.js
@@ -1,6 +1,5 @@
 const express = require('express');
 const app = express();
-const fs = require('fs');
 const data = require('./data.json');
 
 app.get('/api/notes', (req, res) => {
@@ -8,9 +7,40 @@ app.get('/api/notes', (req, res) => {
 });
 
 app.get('/api/notes/:id', (req, res) => {
-  if (req.params.id < 0) res.status(400).json({ error: 'id must be a positive number' });
-  if (!data.notes[req.params.id]) res.status(404).json({ error: 'no notes with that id' });
+  if (req.params.id < 0) {
+    res.status(400).json({ error: 'id must be a positive number' });
+  }
+  if (!data.notes[req.params.id]) {
+    res.status(404).json({ error: 'no notes with that id' });
+  }
   res.status(200).json(data.notes[req.params.id]);
+});
+
+app.use(express.json());
+
+app.post('/api/notes', (req, res) => {
+  if (res.statusCode !== 200) {
+    res.status(500).json({ error: 'an unexpected error occured.' });
+  } else if (req.body.content === '') {
+    res.status(400).json({ error: 'content is a required field' });
+  } else {
+    const noteObj = req.body;
+    noteObj.id = data.nextId;
+    data.notes[data.nextId++] = noteObj;
+    res.status(201).json(noteObj);
+  }
+});
+
+app.delete('/api/notes/:id', (req, res) => {
+  if (req.params.id < 0) {
+    res.status(400).json({ error: 'id must be a positive number' });
+  }
+  if (!data.notes[req.params.id]) {
+    res.status(404).json({ error: 'no notes with that id' });
+  } else {
+    delete data.notes[req.params.id];
+    res.sendStatus(204);
+  }
 });
 
 app.listen(3000, () => {
