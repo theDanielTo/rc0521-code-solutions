413be7007a361de74dcd204a94c3448dfc0f0c7d
theDanielTo <dto1989@gmail.com>
2021-06-29 09:51:23 -0700

    Complete express-post-json

---
+++ express-post-json/index.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const app = express();
+
+let nextId = 1;
+const grades = {};
+
+app.get('/api/grades', (req, res) => {
+  const gradesArr = [];
+  for (const key in grades) gradesArr.push(grades[key]);
+  res.json(gradesArr);
+});
+
+app.use(express.json());
+
+app.post('/api/grades', (req, res) => {
+  grades[nextId] = {
+    name: req.body.name,
+    course: req.body.course,
+    score: req.body.score,
+    id: nextId
+  };
+  nextId++;
+  res.sendStatus(201);
+});
+
+app.listen(3000, () => {
+  // eslint-disable-next-line no-console
+  console.log('Listening on *:3000');
+});
