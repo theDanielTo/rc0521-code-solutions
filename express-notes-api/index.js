4d37f0b30d5ee86d228bd57eee9536111c45a80c
theDanielTo <dto1989@gmail.com>
2021-06-29 16:43:52 -0700

    Client can place a note (PUT) by id

--- express-notes-api/index.js
+++ express-notes-api/index.js
@@ -3,25 +3,26 @@ const app = express();
 const data = require('./data.json');
 
 app.get('/api/notes', (req, res) => {
-  res.json(data.notes);
+  const notesArr = [];
+  for (const key in data.notes) notesArr.push(data.notes[key]);
+  res.json(notesArr);
 });
 
 app.get('/api/notes/:id', (req, res) => {
-  if (req.params.id < 0) {
+  if (req.params.id < 0 || isNaN(req.params.id)) {
     res.status(400).json({ error: 'id must be a positive number' });
-  }
-  if (!data.notes[req.params.id]) {
+  } else if (!data.notes[req.params.id]) {
     res.status(404).json({ error: 'no notes with that id' });
-  }
-  res.status(200).json(data.notes[req.params.id]);
+  } else res.status(200).json(data.notes[req.params.id]);
 });
 
 app.use(express.json());
 
-app.post('/api/notes', (req, res) => {
+app.post('/api/notes', (req, res, err) => {
   if (res.statusCode !== 200) {
-    res.status(500).json({ error: 'an unexpected error occured.' });
-  } else if (req.body.content === '') {
+    throw res.status(500).json({ error: 'an unexpected error occured.' });
+  }
+  if (req.body.content === '') {
     res.status(400).json({ error: 'content is a required field' });
   } else {
     const noteObj = req.body;
@@ -32,14 +33,32 @@ app.post('/api/notes', (req, res) => {
 });
 
 app.delete('/api/notes/:id', (req, res) => {
-  if (req.params.id < 0) {
+  if (res.statusCode !== 200) {
+    res.status(500).json({ error: 'an unexpected error occured.' });
+  } else if (req.params.id < 0 || isNaN(req.params.id)) {
     res.status(400).json({ error: 'id must be a positive number' });
-  }
-  if (!data.notes[req.params.id]) {
+  } else if (!data.notes[req.params.id]) {
     res.status(404).json({ error: 'no notes with that id' });
   } else {
     delete data.notes[req.params.id];
-    res.sendStatus(204);
+    res.status(204).json(req.body);
+  }
+});
+
+app.put('/api/notes/:id', (req, res) => {
+  if (res.statusCode !== 200) {
+    res.status(500).json({ error: 'an unexpected error occured.' });
+  } else if (req.params.id < 0 || isNaN(req.params.id)) {
+    res.status(400).json({ error: 'id must be a positive number' });
+  } else if (req.body.content === '') {
+    res.status(400).json({ error: 'content is a required field' });
+  } else if (!data.notes[req.params.id]) {
+    res.status(404).json({ error: 'no notes with that id' });
+  } else {
+    const noteObj = req.body;
+    noteObj.id = parseInt(req.params.id);
+    data.notes[req.params.id] = noteObj;
+    res.status(200).json(noteObj);
   }
 });
 
