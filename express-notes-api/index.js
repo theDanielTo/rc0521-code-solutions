f1efd203269cdd386904e5f2a47c5cfde0ac7d39
theDanielTo <dto1989@gmail.com>
2021-06-29 13:28:35 -0700

    clients can get a single note by id

--- express-notes-api/index.js
+++ express-notes-api/index.js
@@ -7,6 +7,12 @@ app.get('/api/notes', (req, res) => {
   res.json(data.notes);
 });
 
+app.get('/api/notes/:id', (req, res) => {
+  if (req.params.id < 0) res.status(400).json({ error: 'id must be a positive number' });
+  if (!data.notes[req.params.id]) res.status(404).json({ error: 'no notes with that id' });
+  res.status(200).json(data.notes[req.params.id]);
+});
+
 app.listen(3000, () => {
   // eslint-disable-next-line no-console
   console.log('Listening on *:3000');
