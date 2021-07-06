3f2501e30e7c10461873a7739d15d8d8cbc9cdad
theDanielTo <dto1989@gmail.com>
2021-07-06 15:50:17 -0700

    Removed commented out code

--- es6-classes/stopwatch.js
+++ es6-classes/stopwatch.js
@@ -27,25 +27,3 @@ class Stopwatch {
     this.elapsedSeconds = 0;
   }
 }
-
-// Stopwatch.prototype.tick = function () {
-
-// };
-
-// Stopwatch.prototype.getTime = function () {
-//   const secondsPerHour = 3600;
-//   const secondsPerMinute = 60;
-//   let seconds = this.elapsedSeconds;
-//   let hours = Math.floor(seconds / secondsPerHour);
-//   seconds -= (secondsPerHour * hours);
-//   let minutes = Math.floor(seconds / secondsPerMinute);
-//   seconds -= (secondsPerMinute * minutes);
-//   hours = hours.toString().padStart(2, '0');
-//   minutes = minutes.toString().padStart(2, '0');
-//   seconds = seconds.toString().padStart(2, '0');
-//   return `${hours}:${minutes}:${seconds}`;
-// };
-
-// Stopwatch.prototype.reset = function () {
-//   this.elapsedSeconds = 0;
-// };
