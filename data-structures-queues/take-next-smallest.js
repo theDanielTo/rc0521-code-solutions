00f1fed9238fee1736073affd179bf1eb0d568f4
theDanielTo <dto1989@gmail.com>
2021-08-03 15:55:21 -0700

    Changed loop conditions for takeNextSmallest

--- data-structures-queues/take-next-smallest.js
+++ data-structures-queues/take-next-smallest.js
@@ -3,16 +3,23 @@
 function takeNextSmallest(queue) {
   if (typeof queue.peek() === 'undefined') return;
 
-  let current = queue.dequeue();
+  const container = [];
+  let lastVal = queue.dequeue();
+  container.push(lastVal);
 
-  while (true) {
-    if (typeof queue.peek() !== 'undefined') {
-      if (current < queue.peek()) {
-        return current;
-      } else {
-        queue.enqueue(current);
-        current = queue.dequeue();
-      }
-    } else return current;
+  let found = false;
+  while (typeof queue.peek() !== 'undefined' && !found) {
+    const last = container.length - 1;
+    if (container[last] < queue.peek()) {
+      lastVal = container[last];
+      found = true;
+    } else {
+      container.push(queue.dequeue());
+    }
   }
+
+  container.splice(container.length - 1, 1);
+  for (const elem of container) queue.enqueue(elem);
+
+  return lastVal;
 }
